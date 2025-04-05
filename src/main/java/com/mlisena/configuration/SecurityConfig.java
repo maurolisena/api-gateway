a73c26327a3244c8ccee83a502c61471e939b641
Mauro Lisena <maurolisena@gmail.com>
2025-04-05 11:29:01 -0300

    Updating SecurityConfig to use oauth2ResourceServer method

--- src/main/java/com/mlisena/configuration/SecurityConfig.java
+++ src/main/java/com/mlisena/configuration/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoders;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
@@ -12,11 +14,25 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 public class SecurityConfig {
 
     @Bean
-    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
-        return http
-            .authorizeExchange(auth -> auth.anyExchange().authenticated())
-            .oauth2Login(Customizer.withDefaults())
-            .csrf(ServerHttpSecurity.CsrfSpec::disable)
-            .build();
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchange -> exchange
+                    .pathMatchers("/realms/**").permitAll()
+                    .pathMatchers("/login/**", "/oauth2/**").permitAll()
+                    .pathMatchers("/api/products/**").authenticated()
+                    .anyExchange().permitAll()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                    .jwt(Customizer.withDefaults())
+                );
+
+        return http.build();
+    }
+
+    @Bean
+    public ReactiveJwtDecoder jwtDecoder() {
+        String issuerUri = "http://keycloak-server:8080/realms/application-realm";
+        return ReactiveJwtDecoders.fromIssuerLocation(issuerUri);
     }
 }
